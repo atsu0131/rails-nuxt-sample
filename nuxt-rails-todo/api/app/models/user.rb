58c13e53b0dccf22215121445504071935416637
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-12-31 01:33:31 +0900

    first commit

---
+++ nuxt-rails-todo/api/app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ApplicationRecord
+  has_many :todos # ここを記述
+end
---
+++ nuxt-rails-todo/front/pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-if="user"> <!-- v-ifを追加 -->
+    <p>名前：{{user.name}}</p>
+    <AddTodo @submit="addTodo" />
+    <TodoList :todos="todos" />
+  </div>
+</template>
+
+<script>
+  import AddTodo from "@/components/AddTodo";
+  import TodoList from "@/components/TodoList";
+  import axios from "@/plugins/axios";
+
+  export default {
+    components: {
+      AddTodo,
+      TodoList,
+    },
+    data() {
+      return {
+        todos: [],
+      };
+    },
+    // ここから追加
+    computed: {
+      user() {
+        return this.$store.state.auth.currentUser;
+      }
+    },
+    // ここまで追加
+    created() {
+      console.log("API_KEY:", process.env.API_KEY);
+    },
+    methods: {
+      async addTodo(title) {
+        await axios.post("/v1/todos", { title })
+        this.todos.push({
+          title
+        });
+      },
+    },
+  };
+</script>
+
+
+<style>
+</style>
